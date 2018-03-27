dfa8244adae5fa8aecd447d0adffd0b3149be025
rocciescobar <roccisen.88@gmail.com>
2018-03-27 18:26:11 -0500

    Segundo avance

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import Fancy from 'react-fancy-component';
-import logo from './logo.svg';
+import { BtnSecondary } from 'react-fancy-component';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Probando la guia de estilos...
         </p>
-        <Fancy label="Soy un botón" />
+        <BtnSecondary label="Soy un botón" />
       </div>
     );
   }
